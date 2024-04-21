3539f18ac3bdc2cd7d7a0be5e1b950bb10d089b0
George Hollister <42khane@gmail.com>
2024-04-21 22:21:26 +0100

    feat: added use of tmdb id for movies

--- plex-title-cards.py
+++ plex-title-cards.py
@@ -1,4 +1,5 @@
 import yaml
+import pprint
 import argparse
 from plexapi.server import PlexServer
 
@@ -17,49 +18,60 @@ def upload_poters(yaml_file, plex_url, token):
 
         print(f"Adding to Library '{library_name}'")
             
-        for tvdb_id, data in library_data.items():
-            try:
-                show = library.getGuid(f"tvdb://{tvdb_id}")
-            except:
-                print(f"No TV show found with TVDB ID '{tvdb_id}'. Skipping")
-                continue
+        for id, data in library_data.items():
+            if "seasons" in data:
+                upload_show(id, data, library)
+            else:
+                upload_movie(id, data, library)
 
-            if 'url_poster' in data:
-                show.uploadPoster(data['url_poster'])
-                print(f"Poster uploaded successfully for '{show.title}'")
-            if 'url_background' in data:
-                show.uploadArt(data['url_background'])
-                print(f"Background uploaded successfully for '{show.title}'")
+def upload_show(tvdb_id, show_data, library):
+    show = library.getGuid(f"tvdb://{tvdb_id}")
+    if 'url_poster' in show_data:
+        show.uploadPoster(show_data['url_poster'])
+        print(f"Poster uploaded successfully for '{show.title}'")
+    if 'url_background' in show_data:
+        show.uploadArt(show_data['url_background'])
+        print(f"Background uploaded successfully for '{show.title}'")
 
-            # Assume if there are no numbered seasons that the provided season is the first season
-            seasons_data = data.get('seasons')
-            if sum(isinstance(key, int) for key in seasons_data.keys()) == 0:
-                seasons_data[1] = seasons_data
+    # Assume if there are no numbered seasons that the provided season is the first season
+    seasons_data = show_data.get('seasons')
+    if sum(isinstance(key, int) for key in seasons_data.keys()) == 0:
+        seasons_data[1] = seasons_data
 
-            # Update season posters
-            for season_num, season_data in seasons_data.items():
-                try:
-                    season = show.season(season_num)
-                except:
-                    print(f"Season {season_num} not found for '{show.title}'")
-                    continue
+    for season_number, season_data in seasons_data.items():
+        upload_season(show, season_number, season_data)
 
-                if 'url_poster' in season_data:
-                    season.uploadPoster(season_data['url_poster'])
-                    print(f"Poster uploaded successfully for season {season_num} of '{show.title}'")
+def upload_season(show, season_number, season_data):
+    try:
+        season = show.season(season_number)
+    except:
+        print(f"Season {season_number} not found for '{show.title}'")
+        return
 
-                # Update episode posters
-                for episode_num, episode_data in season_data.get('episodes', {}).items():
-                    try:
-                        episode = season.episode(episode_num)
-                    except:
-                        print(f"No episode found for S{season_num}E{episode_num} of '{show.title}'")
-                        continue
+    if 'url_poster' in season_data:
+        season.uploadPoster(season_data['url_poster'])
+        print(f"Poster uploaded successfully for season {season_number} of '{show.title}'")
 
-                    if 'url_poster' in episode_data:
-                        episode.uploadPoster(episode_data['url_poster'])
-                        print(f"Poster uploaded successfully for S{season_num}E{episode_num} of '{show.title}'")
+    # Update episode posters
+    for episode_number, episode_data in season_data.get('episodes', {}).items():
+        try:
+            episode = season.episode(episode_number)
+        except:
+            print(f"No episode found for S{season_number}E{episode_number} of '{show.title}'")
+            continue
 
+        if 'url_poster' in episode_data:
+            episode.uploadPoster(episode_data['url_poster'])
+            print(f"Poster uploaded successfully for S{season_number}E{episode_number} of '{show.title}'")
+    
+def upload_movie(tmdb_id, movie_data, library):
+    movie = library.getGuid(f"tmdb://{tmdb_id}")
+    if 'url_poster' in movie_data:
+        movie.uploadPoster(movie_data['url_poster'])
+        print(f"Poster uploaded successfully for '{movie.title}'")
+    if 'url_background' in movie_data:
+        movie.uploadArt(movie_data['url_background'])
+        print(f"Background uploaded successfully for '{movie.title}'")
 # Example usage
 if __name__ == "__main__":
 
